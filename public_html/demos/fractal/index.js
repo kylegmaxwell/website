e57f6f7c85e0fd163fb6edcf1f0c329fe9a94dd6
kylegmaxwell <kyle.g.maxwell@gmail.com>
2016-03-26 15:37:33 -0700

    Added line drawing fractals
    New mode to draw lines between current and prev points. Also found bug
    in particle iteration was not checking for out of bounds, so numbers
    diverged to NaN causing slowness and badness.

--- public_html/demos/fractal/index.js
+++ public_html/demos/fractal/index.js
@@ -9,6 +9,11 @@ var paintMode = false;
  * Main function that runs onLoad for the page
  */
 function handleLoad() {
+    var mode = localStorage.getItem('fractal.mode');
+    if (mode != null) {
+        modeSelector.value = mode;
+    }
+    gameCanvas.width = 500;
     gameCanvas.height = gameCanvas.width;
     ctx = gameCanvas.getContext('2d');
     resetGame();
@@ -16,63 +21,6 @@ function handleLoad() {
     gameCanvas.addEventListener('mousedown', handleMouse, false);
     gameCanvas.addEventListener('mousemove', handleMouse, false);
     gameCanvas.addEventListener('mouseup', handleMouse, false);
-}
-
-function render() {
-    var width = gameCanvas.width;
-    var height = gameCanvas.height;
-    var imageData = ctx.getImageData(0, 0, width, height);
-    var data = imageData.data;
-
-    if (renderObj) {
-        renderObj.render(data);
-    }
-
-    ctx.putImageData(imageData, 0, 0);
-}
-
-function getIter() {
-    var valueString = iterInput.value;
-    var value = DEFAULT_ITER;
-    if (!isNaN(valueString)) {
-        value = Number(valueString);
-    }
-    return value;
-}
-
-function updateIter() {
-    if (renderObj)
-        renderObj.iter = getIter();
-    iterInput.value = renderObj.iter;
-    render();
-
-}
-
-function incIter() {
-    if (renderObj)
-        renderObj.iter++;
-    iterInput.value = renderObj.iter;
-    render();
-}
-
-function resetGame() {
-    paintMode = !!paintCheck.checked;
-
-    var width = gameCanvas.width;
-    var height = gameCanvas.height;
-    var iter = getIter();
-    switch(parseInt(modeSelector.value)) {
-        case 0: // Mandelbrot
-            renderObj = new Mandelbrot(height, width, iter, paintMode);
-            break;
-        case 1: // Buddhabrot
-            renderObj = new Buddhabrot(height, width, iter, paintMode);
-            break;
-        case 2: // Partibrot
-            renderObj = new Partibrot(height, width, iter, paintMode);
-            break;
-    }
-    render();
 }
 
 var inDrag = false;
@@ -91,4 +39,89 @@ function handleMouse(e) {
         ctx.fill();
     }
     render();
+}
+
+function getIter() {
+    var valueString = iterInput.value;
+    var value = DEFAULT_ITER;
+    if (!isNaN(valueString)) {
+        value = Number(valueString);
+    }
+    return value;
+}
+
+function updateT() {
+    var valueString = testInput.value;
+    var value = 0;
+    if (!isNaN(valueString)) {
+        value = Number(valueString);
+    }
+    if (renderObj)
+        renderObj.t = value;
+    render();
+}
+
+
+function updateIter() {
+    if (renderObj)
+        renderObj.iter = getIter();
+    iterInput.value = renderObj.iter;
+    render();
+}
+
+function incIter() {
+    if (renderObj)
+        renderObj.iter++;
+    iterInput.value = renderObj.iter;
+    render();
+}
+
+function doClear() {
+   ctx.clearRect(0,0,gameCanvas.width, gameCanvas.height);
+}
+
+function resetGame() {
+    paintMode = !!paintCheck.checked;
+
+    var width = gameCanvas.width;
+    var height = gameCanvas.height;
+    var iter = getIter();
+    /*
+    More ideas
+        1. Interpolate particles between fixed steps
+        2. loft curves along interpolated points with increasing z
+        3. Color by delta position, relative to previous frame, or start position
+    */
+    switch(parseInt(modeSelector.value)) {
+        case 0: // Mandelbrot
+            renderObj = new Mandelbrot(height, width, iter, paintMode);
+            break;
+        case 1: // Buddhabrot
+            renderObj = new Buddhabrot(height, width, iter, paintMode);
+            break;
+        case 2: // Partibrot
+            renderObj = new Partibrot(height, width, iter, paintMode);
+            break;
+        case 3: // Linebrot
+            renderObj = new Linebrot(height, width, iter, paintMode);
+            break;
+    }
+    render();
+}
+
+function render() {
+    var width = gameCanvas.width;
+    var height = gameCanvas.height;
+    var imageData = ctx.getImageData(0, 0, width, height);
+    var data = imageData.data;
+
+    if (renderObj) {
+        if (renderObj.render) {
+            renderObj.render(data);
+            ctx.putImageData(imageData, 0, 0);
+        } else {
+            renderObj.draw(ctx);
+        }
+    }
+    localStorage.setItem('fractal.mode', parseInt(modeSelector.value));
 }
--- public_html/demos/fractal/linebrot.js
+++ public_html/demos/fractal/linebrot.js
@@ -1,15 +1,14 @@
 'use strict';
 
 /**
- * Class to track points moving through space driven by the mandelbrot iteration.
+ * Class to draw lines between iterations of the mandelbrot set.
  * @param {Number} height The number of vertical pixels
  * @param {Number} width The number of horizontal pixels
  * @param {Number} iter The maximum number of iterations
  * @param {Boolean} paintMode Whether to use user defined start points
  */
-function Partibrot(height, width, iter, paintMode) {
+function Linebrot(height, width, iter, paintMode) {
 
-    this.steps = 10;
     this.height = height;
     this.width = width;
     this.iter = iter;
@@ -24,53 +23,21 @@ function Partibrot(height, width, iter, paintMode) {
     this.initBuffer(this.particles);
     this.checkIter();
 
+    // this.colorBuffer = [];
     this.paintBuffer = [];
 }
 
-
 /**
- * Ease in out animation retiming
- *
- * Source http://gizma.com/easing/
- *
- * @param  {Number} t The time
- * @param  {Number} b Start value
- * @param  {Number} c Change in value
- * @param  {Number} d Duration
- * @return {Number}   The new time
+ * Step through iterations of mandelbrot.
  */
-Partibrot.prototype.easeInOutQuad = function (t, b, c, d) {
-    t /= d/2;
-    if (t < 1) return c/2*t*t + b;
-    t--;
-    return -c/2 * (t*(t-2) - 1) + b;
-};
+Linebrot.prototype.iterate = function () {
 
-/**
- * Step through iterations and/or interpolations.
- *
- * Uses this.steps to determine whether the next frame
- * is an interpolation or an iteration.
- * Every step frames we do an iteration.
- * Otherwise we just update t
- */
-Partibrot.prototype.iterate = function () {
-    // Determine the iteration number
-    var iterTmp = Math.floor(this.iter/this.steps);
-    var prevIterTmp = Math.floor(this.prevIter/this.steps);
-
-    // Iterate and store previous result
-    for (var i=prevIterTmp;i<iterTmp;i++) {
+    for (var i=this.prevIter;i<this.iter;i++) {
         var tmp = this.particles;
         this.stepBuffer(this.particles, this.prevParticles);
         this.particles = this.prevParticles;
         this.prevParticles = tmp; // Reuse the array
     }
-
-    // Calculate iteration t
-    this.t = Math.max(Math.min((this.iter%this.steps)*(1/this.steps),1),0);
-    this.t = this.easeInOutQuad(this.t, 0, 1, 1);
-
     this.prevIter = this.iter;
 }
 /**
@@ -78,7 +45,7 @@ Partibrot.prototype.iterate = function () {
  * Contents are [x position, y position, starting x, starting y]
  * @param  {Array} particleBuffer The buffer to populate
  */
-Partibrot.prototype.initBuffer = function(particleBuffer) {
+Linebrot.prototype.initBuffer = function(particleBuffer) {
     var r, c;
     var index=0;
     var a=-2, b=-2;
@@ -99,9 +66,10 @@ Partibrot.prototype.initBuffer = function(particleBuffer) {
  * Iterate the particles with one mandelbrot step
  * @param {Array} particleBuffer Particle array (see initBuffer)
   */
-Partibrot.prototype.stepBuffer = function(inBuffer, outBuffer) {
+Linebrot.prototype.stepBuffer = function(inBuffer, outBuffer) {
     var r, c;
     var index=0;
+
     for (r=0;r<this.height;r++)
     {
         for (c=0;c<this.width;c++)
@@ -129,24 +97,24 @@ Partibrot.prototype.stepBuffer = function(inBuffer, outBuffer) {
  * @param  {Number} i Seed
  * @return {Array.<Number>}   Color r, g, b 0-255
  */
-Partibrot.prototype.color = function(i) {
+Linebrot.prototype.color = function(i) {
     return [i%200+30, ((i+234567)%7)*24+24, (i%57)+75];
 };
 
 /**
- * Calculate the mandlebrot set with n iterations
- * {Uvar8ClampedArray} pixels The r, g, b, a pixel data (0 to 255)
+ * Draw the curves between the current and previous set.
+ * {CanvasRenderingContext2D} ctx The canvas drawing context
  */
-Partibrot.prototype.render = function (pixels) {
+Linebrot.prototype.draw = function (ctx) {
     this.checkIter();
     var h4 = (this.height-1)/4.0, w4 = (this.width-1)/4.0, h2 = this.height/2.0, w2 = this.width/2.0;
     var a=-2, b=-2, x=0, y=0, x2=0, y2=0;
-    var r,c,i,j,rr,cc;
+    var r,c,i,j,rr1,cc1,rr2,cc2;
     var index=0, countIndex=0;
     var count=[];
-    var cx, cy, px, py;
+    var cx, cy, px, py, mx, my;
     countIndex=0;
-    // Sample / render each particle into a sparse array
+
     for (countIndex=0;countIndex<this.particles.length;countIndex++) {
         //initialize positions
         var pos = this.particles[countIndex];
@@ -155,39 +123,27 @@ Partibrot.prototype.render = function (pixels) {
         var ppos = this.prevParticles[countIndex];
         px = ppos[0];
         py = ppos[1];
-        var t = this.t;
-        var tm = 1.0-t;
-        x = tm * px + t * cx;
-        y = tm * py + t * cy;
-        if (x*x+y*y<4) {
-            //compute location in count array
-            rr = Math.floor(y*(h4)+(h2));
-            cc = Math.floor(x*(w4)+(w2));
-            //ignore first iteration
-            index = rr*this.width+cc;
+        mx = 0.5 * cx + 0.5 * px;
+        my = 0.5 * cy + 0.5 * py;
+        if (cx*cx+cy*cy<4 || px*px+py*py<4) {
+
             var color = this.color(countIndex);
-            if (count[index]) {
-                count[index][0] += color[0];
-                count[index][1] += color[1];
-                count[index][2] += color[2];
-            } else {
-                count[index]=color;
-            }
-        }
-    }
-    index=0;
-    countIndex=0;
-    for (r=0;r<this.height;r++)
-    {
-        for (c=0;c<this.width;c++)
-        {
-            var j = count[countIndex]||[0,0,0];
-            pixels[index+0] = j[0];
-            pixels[index+1] = j[1];
-            pixels[index+2] = j[2];
-            pixels[index+3] = toFixed(1);
-            countIndex++;
-            index+=4;
+            rr1 = Math.floor(py*(h4)+(h2));
+            cc1 = Math.floor(px*(w4)+(w2));
+            rr2 = Math.floor(cy*(h4)+(h2));
+            cc2 = Math.floor(cx*(w4)+(w2));
+
+            var dy = (rr2-rr1);
+            var dx = (cc2-cc1);
+            var len2 = dx*dx+dy*dy;
+            var alpha = (10000 - len2)*0.0001;
+            alpha *= 1.0-((mx*mx+my*my)*0.25);
+            var colorStr = "rgba("+color[0]+","+color[1]+","+color[2]+","+alpha+")"
+            ctx.strokeStyle = colorStr;
+            ctx.beginPath();
+            ctx.moveTo(cc1,rr1);
+            ctx.lineTo(cc2,rr2);
+            ctx.stroke();
         }
     }
 };
@@ -197,8 +153,7 @@ Partibrot.prototype.render = function (pixels) {
  *
  * Simulates up to the appropriate frame based on the delta.
  */
-Partibrot.prototype.checkIter = function () {
-
+Linebrot.prototype.checkIter = function () {
     if (this.iter < this.prevIter) {
         this.prevIter = 0;
         this.initBuffer(this.particles);
